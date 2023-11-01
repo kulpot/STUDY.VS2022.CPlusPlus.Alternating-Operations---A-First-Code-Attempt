ac945aeff4217b989680e7b2af563855ecd98092
Kulpot <kulpotwahu@gmail.com>
2023-11-01 22:34:35 +0800

    https://www.youtube.com/watch?v=B2H-n0P8vhY&list=PLRwVmtr-pp05c1HTBj1no6Fl6C6mlxYDG&index=34

--- Alternating Operations - A First Code Attempt/Mainer.cpp
+++ Alternating Operations - A First Code Attempt/Mainer.cpp
@@ -1,5 +1,5 @@
 
-//ref link:
+//ref link:https://www.youtube.com/watch?v=B2H-n0P8vhY&list=PLRwVmtr-pp05c1HTBj1no6Fl6C6mlxYDG&index=34
 //
 
 // MyAssembly.asm - (customBuildTool)commandline - ml /c /Cx /coff "%(FullPath)"
